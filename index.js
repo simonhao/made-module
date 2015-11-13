010a8226cfc7a970324cbb92610858a963989765
IetnHao <dreamschool@163.com>
2015-11-13 11:29:36 +0800

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Made-Module
+ * @author: SimonHao
+ * @date:   2015-11-13 11:27:55
+ */
+
+'use strict';
+
+function MadeModule(module_id, instance){
+
+}
+
+
+MadeModule.prototype._made_module = true;
+
+
+module.exports = MadeModule;
